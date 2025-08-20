b60579a704c0af799874a2cf0f07f0d07b39816e
Haya <haya.ehab.dev@gmail.com>
2025-08-20 08:26:50 +0300

    Fixed Issues In App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders instructor dashboard route', () => {
+  render(
+    <MemoryRouter initialEntries={['/instructor-dashboard']}>
+      <App />
+    </MemoryRouter>
+  );
+  const element = screen.getByText(/instructor dashboard/i);
+  expect(element).toBeInTheDocument();
 });
